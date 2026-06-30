ee984f5afc61dc2f4b18654df26a582040c0933e
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-06-30 23:39:30 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,20 @@
+class max
+{
+    public static void main(String args[])
+    {
+        int a = 50, b = 45, c = 5;
+
+        if(a > b && a > c)
+        {
+            System.out.println("a is Maximum");
+        }
+        else if(b > a && b > c)
+        {
+            System.out.println("b is Maximum");
+        }
+        else
+        {
+            System.out.println("c is Maximum");
+        }
+    }
+}
