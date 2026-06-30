15bfe3167faa099da964bcef4c1d518b60690bbf
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-06-30 23:39:52 +0530

    Add files via upload

---
+++ min.java
@@ -0,0 +1,20 @@
+class min
+{
+    public static void main(String args[])
+    {
+        int a = 50, b = 45, c = 5;
+
+        if(a < b && a < c)
+        {
+            System.out.println("a is Minimum");
+        }
+        else if(b < a && b < c)
+        {
+            System.out.println("b is Minimum");
+        }
+        else
+        {
+            System.out.println("c is Minimum");
+        }
+    }
+}
