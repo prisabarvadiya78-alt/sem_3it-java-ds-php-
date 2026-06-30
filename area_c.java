f500bdb4738fcafd262a6330b20522e61b09ba8c
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-06-30 23:39:10 +0530

    Add files via upload

---
+++ area_c.java
@@ -0,0 +1,12 @@
+class area_C
+{
+    public static void main(String args[])
+    {
+        int r = 5;
+        double a_c;
+
+        a_c = 3.14 * r * r;
+
+        System.out.println("Area of Circle = " + a_c);
+    }
+}
