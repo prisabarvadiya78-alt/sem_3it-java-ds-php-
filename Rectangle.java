9ec2522f3726aff34f5bd22fa090a857e6612c5f
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-06-30 23:40:13 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+class Rectangle
+{
+    public static void main(String args[])
+    {
+        double a = 10;
+        double b = 5;
+
+        double area;
+        area = a * b;
+
+        System.out.println("Area of Rectangle = " + area);
+    }
+}
