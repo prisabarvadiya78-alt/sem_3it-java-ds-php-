cabc86f7a1687abfc026e28adc90b5a0edb5e10a
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-07-06 22:36:27 +0530

    Add files via upload

---
+++ foreach_loop.java
@@ -0,0 +1,17 @@
+class foreach_loop
+{
+  public static void main(String args[])
+  {
+  int numbers[]={10,20,30,40,50};
+  for(int x:numbers)
+  {
+  System.out.println(x+",");
+  }
+  System.out.println();
+  String names[]={"james","larray","tom","lacy"};
+  for(String name:names)
+  {
+  System.out.print(name+",");
+  }
+}
+}
