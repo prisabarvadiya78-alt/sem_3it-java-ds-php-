8612f6e968a965a659533c58ffba2d7a53df22ec
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-07-20 14:48:27 +0530

    Add files via upload

---
+++ exit.php
@@ -0,0 +1,16 @@
+<?php
+$a=10;
+$b=10;
+if($a==$b)
+{
+    echo "this is test <br>";
+    echo "both are same <br>";
+
+    echo $a+$b;
+    exit();
+}
+else
+{
+    exit();
+}
+?>
---
+++ header.php
@@ -0,0 +1,8 @@
+<?php
+header("Location:exit.php");
+//header('Content-Type: application/pdf');
+//header('Content-Disposition: attachment; filename="downloaded.pdf"');
+//readfile('original.pdf');
+echo "this is called header";
+
+?>
