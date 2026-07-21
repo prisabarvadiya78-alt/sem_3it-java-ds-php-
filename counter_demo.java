d21523dcff9681a27ffbb90d9f96035ee7bf10ca
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-07-21 17:51:16 +0530

    Add files via upload

---
+++ counter_demo.java
@@ -0,0 +1,17 @@
+class counter_demo 
+{
+    static int count = 0; // memory only once and retain its value
+
+    counter_demo() 
+	{
+        count++;
+        System.out.println(count);
+    }
+
+    public static void main(String args[])
+	{
+        counter_demo c1 = new counter_demo();
+        counter_demo c2 = new counter_demo();
+        counter_demo c3 = new counter_demo();
+    }
+}
