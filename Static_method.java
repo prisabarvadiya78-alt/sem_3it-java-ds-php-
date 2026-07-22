d7f98864a3d1e29e2bd3cb5ad24fea34aab7bb64
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-07-22 13:45:45 +0530

    Add files via upload

---
+++ Static_method.java
@@ -0,0 +1,21 @@
+// Example of static method
+
+class calculate 
+{
+    static int cube(int x)
+
+	{
+        return x * x * x;
+    }
+}
+
+class Static_method 
+{
+    public static void main(String args[]) 
+	{
+        int result = calculate.cube(5);
+        System.out.println(result);
+        result = calculate.cube(10);
+        System.out.println(result);
+    }
+}
