515cee814b57add914e3f1952679033e3a10952d
prisabarvadiya78-alt <prisabarvadiya78@gmail.com>
2026-07-24 13:56:07 +0530

    Add files via upload

---
+++ MultiDemo.java
@@ -0,0 +1,36 @@
+// Example of Multilevel Inheritance
+
+class X
+ {
+    public void methodX() 
+	{
+        System.out.println("Class X method");
+    }
+}
+
+class Y extends X
+ {
+    public void methodY()
+	{
+        System.out.println("Class Y method");
+    }
+}
+
+class Z extends Y 
+{
+    public void methodZ()
+	{
+        System.out.println("Class Z method");
+    }
+}
+
+class MultiDemo
+ {
+    public static void main(String args[])
+	{
+        Z obj = new Z();
+        obj.methodX();
+        obj.methodY();
+        obj.methodZ();
+    }
+}
---
+++ Superkeyword.java
@@ -0,0 +1,23 @@
+class A {
+    int a = 10;
+}
+
+class B extends A 
+{
+    int a = 20;
+
+    public void display()
+	{
+        System.out.println("Value : " + a);
+        System.out.println("Value : " + super.a);
+    }
+}
+
+class Superkeyword 
+{
+    public static void main(String args[])
+	{
+        B obj = new B();
+        obj.display(); // prints value of local a variable
+    }
+}
